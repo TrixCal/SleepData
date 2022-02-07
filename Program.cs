9c75e1b49c773efde06830a07c710ff83c7e68b8
TrixCal <traytonlarson01@gmail.com>
2022-02-07 12:02:27 -0600

    Parse Data from File

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
+            //file name
+            string file = "data.txt";
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
@@ -34,7 +36,7 @@ namespace SleepData
                 Random rnd = new Random();
 
                 // create file
-                StreamWriter sw = new StreamWriter("data.txt");
+                StreamWriter sw = new StreamWriter(file);
                 // loop for the desired # of weeks
                 while (dataDate < dataEndDate)
                 {
@@ -55,7 +57,32 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                if(File.Exists(file)){
+                    StreamReader sr = new StreamReader(file);
+                    while(!sr.EndOfStream){
+                        string ln = sr.ReadLine();
+                        string[] week = ln.Split(",");
+                        
+                        DateTime date = DateTime.Parse(week[0]);
+                        string[] hours = week[1].Split("|");
+                        int total = 0;
+                        int num = 0;
+                        foreach(string h in hours){
+                            total += int.Parse(h);
+                            num++;
+                        }
+                        double average = (double)total/num;
+                        Console.WriteLine($"Week of {date:MMM}, {date:dd}, {date:yyyy}");
+                        Console.WriteLine($"{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}{"Su",3}{"Tot",4}{"Avg",4}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"---",4}{"---",4}");
+                        Console.WriteLine($"{hours[0],3}{hours[1],3}{hours[2],3}{hours[3],3}{hours[4],3}{hours[5],3}{hours[6],3}{total,4}{average,4:n1}");
+                        Console.WriteLine();
+                    }
+                    sr.Close();
+                }
+                else{
+                    Console.WriteLine("File doens't exist");
+                }
 
             }
         }
